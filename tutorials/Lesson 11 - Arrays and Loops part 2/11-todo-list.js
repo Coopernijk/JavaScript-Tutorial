9a02e0ac733d0fd0a8ca5cf71329e93879ff3643
Coopernijk <hs21hidayat@gmail.com>
2026-08-21 12:27:28 +0800

    Chapter 11 part 2 DONE

---
+++ tutorials/Lesson 11 - Arrays and Loops part 2/11-todo-list.js
@@ -0,0 +1,60 @@
+
+/* 
+algorithm:
+- Create an empty array to store todos
+- User clicked 'Add'
+- Fetch text from textbox
+- Add it to array
+- Console.log() the array
+- reset inputbox
+*/
+
+const todoList = [{
+  name: 'make dinner', 
+  dueDate: '2022-12-22'
+},{
+  name: 'wash dishes',
+  dueDate: '2022-12-22'
+}];
+
+renderTodoList ();
+
+function renderTodoList () {
+  let todoListHTML = '';
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate
+    
+    // destructure
+    const {name, dueDate} = todoObject;
+    const html = `
+      <div>${name}</div> 
+      <div>${dueDate}</div> 
+        <button onclick="
+          todoList.splice(${i}, 1);
+          renderTodoList();
+        " class="delete-todo-button">Delete</button>`;
+    todoListHTML += html;
+  }
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate
+    // shorthand version
+    name, dueDate
+  });
+
+  inputElement.value = '';
+  renderTodoList ();
+}
